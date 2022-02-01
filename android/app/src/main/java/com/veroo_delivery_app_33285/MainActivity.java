e097ff71e595f3063891b822f0248d7456f9fc72
Crowdbotics <team@crowdbotics.com>
2022-02-01 14:16:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/veroo_delivery_app_33285/MainActivity.java
@@ -0,0 +1,15 @@
+package com.veroo_delivery_app_33285;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "veroo_delivery_app_33285";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "veroo_delivery_app_33285",
+  url: "https://veroo_delivery_app_33285.botics.co",
+  api: "https://veroo_delivery_app_33285.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
